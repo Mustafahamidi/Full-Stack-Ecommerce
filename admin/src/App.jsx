acfcec162f7f02360c40130976a41c3a1c565eae
Mustafa <mmustafakhan260@gmail.com>
2025-05-28 21:18:19 +0430

    Fix file name casing for imports

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,32 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
-import SideBar from './components/SideBar.jsx'
+import SideBar from './components/SideBar'
 import Add from './pages/Add'
 import List from './pages/List'
-import Orders from './pages/Orders.jsx'
+import Orders from './pages/Orders'
 import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
-
 
 import { ToastContainer } from 'react-toastify';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL
 export const currency = '$'
+
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'): "")
+  const [token, setToken] = useState(localStorage.getItem('token') || "")
   
   useEffect(() => {
-    localStorage.setItem('token',token)
-  },[token])
+    localStorage.setItem('token', token)
+  }, [token])
 
   return (
     <div className='bg-gray-50 min-h-screen'>
       <ToastContainer />
-      {token === '' ? <Login setToken={setToken}/> :
+      {token === '' ? (
+        <Login setToken={setToken} />
+      ) : (
         <>
-          <Navbar setToken = {setToken}/>
+          <Navbar setToken={setToken} />
           <hr />
-          <div className='flex w-ful'>
+          <div className='flex w-full'>  {/* Fixed typo from w-ful */}
             <SideBar />
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
@@ -36,10 +38,8 @@ const App = () => {
               </Routes>
             </div>
           </div>
-
         </>
-      }
-
+      )}
     </div>
   )
 }
