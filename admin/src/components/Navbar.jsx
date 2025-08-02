f6896b3e11407fbd54397f428aa835ad04250c4b
Mustafa Hamidi <mmustafakhan260@gmail.com>
2025-08-02 11:19:08 +0430

    fix logo

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { assets } from '../assets/assets.js'
 const Nabar = ({setToken}) => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
-        <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+    <div className='flex items-center px-[4%] justify-between '>
+        <img className='w-[max(10%,80px)] w-30' src={assets.logo_hs} alt="" />
         <button onClick={() => setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
